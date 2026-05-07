00ab0eb8db1f86140371b963f2a0b6c62b927e3a
Aizhan <aizhan.zhumashova@gmail.com>
2026-05-07 14:24:51 +0500

    Add base user entity and role enum

---
+++ backend/src/main/java/com/dormitory/backend/entity/User.java
@@ -0,0 +1,25 @@
+package com.dormitory.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
